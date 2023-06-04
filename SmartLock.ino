655e6b2963cacfa2e5905ec1b08c452f6041a710
trhiep <76655009+trhiep@users.noreply.github.com>
2023-06-05 00:08:52 +0700

    Add files via upload

--- SmartLock.ino
+++ SmartLock.ino
@@ -21,11 +21,18 @@ byte colPins[COLS] = {A3, A2, A1, A0};    // Kết nối các cột từ chân 5
 
 int defaultPassword1 = 1234;
 int defaultPassword2 = 9999;
-
 int systemPassword1;
 int systemPassword2;
-String systemUID = "0228581a";
+String UIDMaster = "0228581a";
+String secondUID = "1f07d0bd";
 int delayTimeDefault = 3000;
+
+int attempts = 3;
+int lockMins = 1;
+boolean isWarning = false;
+int lockDelay = 1000;
+int mins = 0;
+int secs = 0;
 
 int lastDoorState = 0;
 int buzzerPin = 8;
@@ -62,37 +69,10 @@ void setup() {
 }
 
 void loop() {
-
   lcd.setCursor(0, 0);
   lcd.print("Smart Lock - GR7");
   // Đọc mã RFID
-  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
-    // Lấy thông tin mã RFID
-    String scannedUID = "";
-    for (byte i = 0; i < rfid.uid.size; i++) {
-      scannedUID.concat(String(rfid.uid.uidByte[i] < 0x10 ? "0" : ""));
-      scannedUID.concat(String(rfid.uid.uidByte[i], HEX));
-    }
-
-    Serial.print("[RFID] --> Scanned UID: ");
-    Serial.println(scannedUID);
-    // Kiểm tra mã RFID
-    if (checkRFID(scannedUID)) {
-      accessDoor();
-      delay(delayTimeDefault);
-      lcd.clear();
-      lcd.print("Smart Lock - GR7");
-    } else {
-      lcd.clear();
-      lcd.print("Wrong RFID card!");
-      delay(delayTimeDefault);
-      lcd.clear();
-      lcd.print("Smart Lock - GR7");
-    }
-
-    rfid.PICC_HaltA();
-    rfid.PCD_StopCrypto1();
-  }
+  scanRFID();
 
   // Kiểm tra keypad
   char key = keypad.getKey();
@@ -109,12 +89,49 @@ void loop() {
 }
 
 
+void scanRFID() {
+  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    // Lấy thông tin mã RFID
+    String scannedUID = "";
+    for (byte i = 0; i < rfid.uid.size; i++) {
+      scannedUID.concat(String(rfid.uid.uidByte[i] < 0x10 ? "0" : ""));
+      scannedUID.concat(String(rfid.uid.uidByte[i], HEX));
+    }
+
+    Serial.print("[RFID] --> Scanned UID: ");
+    Serial.println(scannedUID);
+    // Kiểm tra mã RFID
+    if (checkRFID(scannedUID)) {
+      accessDoor();
+      delay(delayTimeDefault);
+      lcd.clear();
+      lcd.print("Smart Lock - GR7");
+
+    } else {
+      lcd.clear();
+      lcd.print("Wrong RFID card!");
+      delay(delayTimeDefault);
+      lcd.clear();
+      lcd.print("Smart Lock - GR7");
+    }
+
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();
+  }
+}
+
 
 boolean checkRFID(String scannedUID) {
   // Kiểm tra mã RFID với danh sách mã được chấp nhận
-  // Ở đây chỉ có một mã chính
-  if (scannedUID.equals(systemUID)) {
+  if (scannedUID.equals(UIDMaster) || scannedUID.equals(secondUID)) {
     Serial.println("[SYSTEM] --> Correct UID.");
+    attempts = 3;
+    if (isWarning == true) {
+      mins = 0;
+      secs = 0;
+    }
+    isWarning = false;
+    lockMins = 1;
     return true;
   }
   Serial.println("[SYSTEM] --> Wrong UID.");
@@ -122,19 +139,64 @@ boolean checkRFID(String scannedUID) {
 }
 
 void checkPass() {
-  int enteredPassword = enterPass("Enter password:");
-  Serial.print("[KEYPAD] --> User enterd: ");
-  Serial.println(enteredPassword);
-  if (enteredPassword == systemPassword1 || enteredPassword == systemPassword2) {
-    Serial.println("[SYSTEM] --> Correct password.");
-    accessDoor();
-    delay(delayTimeDefault);
-  } else {
+  if (attempts == 0) {
     lcd.clear();
-    lcd.print("Wrong password!");
-    Serial.println("[SYSTEM] --> Wrong password.");
-    delay(delayTimeDefault);
+    lcd.setCursor(0, 0);
+    lcd.print("Try again in:");
+    for (mins = lockMins - 1; mins >= 0; mins--) {
+      for (secs = 59; secs >= 0; secs--) {
+        lcd.setCursor(5, 1);
+        lcd.print(mins);
+        lcd.setCursor(7, 1);
+        lcd.print(":");
+        lcd.setCursor(8, 1);
+        lcd.print(secs);
+        delay(lockDelay);
+        scanRFID();
+        lcd.setCursor(0, 1);
+        lcd.print("                ");
+      }
+    }
+
+    if (isWarning == true) {
+      lockMins = lockMins * 2;
+      if (lockMins > 59) {
+        lockMins = 59;
+      }
+    }
+    attempts = 1;
+    lcd.clear();
+
+  } else {
+    int enteredPassword = enterPass("Enter password:");
+    Serial.print("[KEYPAD] --> User enterd: ");
+    Serial.println(enteredPassword);
+    if (enteredPassword == systemPassword1 || enteredPassword == systemPassword2) {
+      Serial.println("[SYSTEM] --> Correct password.");
+      accessDoor();
+      attempts = 3;
+      isWarning = false;
+      lockMins = 1;
+      delay(delayTimeDefault);
+    } else {
+      lcd.clear();
+      lcd.print("Wrong password!");
+      Serial.println("[SYSTEM] --> Wrong password.");
+      attempts--;
+      if (attempts == 0) {
+        isWarning = true;
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Disable unlock");
+        lcd.setCursor(0, 1);
+        lcd.print("by password!");
+        warningSound();
+      }
+      delay(delayTimeDefault);
+      lcd.clear();
+    }
   }
+
 }
 
 int enterPass(String msg) {
@@ -194,7 +256,7 @@ void changePass() {
   char key;
   do {
     key = keypad.getKey();
-    
+
     lcd.setCursor(0, 0);
     lcd.print("1. Change pass 1");
     lcd.setCursor(0, 1);
@@ -244,19 +306,19 @@ void changePass() {
 }
 
 void resetPass() {
-  
+
   EEPROM.writeInt(10, defaultPassword1);
   delay(100);
   EEPROM.writeInt(100, defaultPassword2);
   delay(100);
   Serial.println("[SYSTEM] --> Password reseted.");
-  
+
   systemPassword1 = EEPROM.readInt(10);
   delay(100);
   systemPassword2 = EEPROM.readInt(100);
   delay(100);
   Serial.println("[SYSTEM] --> Reloaded password.");
-  
+
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Reset password");
@@ -269,5 +331,27 @@ void resetPass() {
 void enterPassSound() {
   digitalWrite(buzzerPin, HIGH);
   delay(20);
+  digitalWrite(buzzerPin, LOW);
+}
+
+void warningSound() {
+  digitalWrite(buzzerPin, HIGH);
+  delay(1000);
+  digitalWrite(buzzerPin, LOW);
+  delay(100);
+  digitalWrite(buzzerPin, HIGH);
+  delay(1000);
+  digitalWrite(buzzerPin, LOW);
+  delay(100);
+  digitalWrite(buzzerPin, HIGH);
+  delay(1000);
+  digitalWrite(buzzerPin, LOW);
+  delay(100);
+  digitalWrite(buzzerPin, HIGH);
+  delay(1000);
+  digitalWrite(buzzerPin, LOW);
+  delay(100);
+  digitalWrite(buzzerPin, HIGH);
+  delay(1000);
   digitalWrite(buzzerPin, LOW);
 }
